0aa21b20f854cf2116e7d723af8646d337900892
mkolksas <terermichael2@gmail.com>
2025-02-03 16:10:16 +0300

    python

---
+++ introduction.py
@@ -0,0 +1,7 @@
+#Print is a method used to display an output
+
+print("Michael")
+print("eMobilis")
+print(100)
+print(10+20)
+
---
+++ variable.py
@@ -0,0 +1,19 @@
+#Variable is a container used to contain a value
+
+location = "Westlands"
+firstname = "Michael"
+lastname = "Murgor"
+school = "eMobilis"
+x = 39
+y = 2
+
+print(location)
+print(firstname)
+print(lastname)
+print(school)
+print(firstname, lastname)
+print("My full name is",firstname,lastname)
+print(school,"is a coding school")
+print(x + y)
+#The product of 39 and 2 is 78
+print("The product of",x,"and",y,"is",x * y)
