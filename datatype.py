ce969ebb959a3192694816abdee274019d42c693
maeriii <mwanziaedwin5@gmail.com>
2025-02-04 16:14:25 +0300

    python

---
+++ datatype.py
@@ -0,0 +1,28 @@
+number = 50 #Int
+num = 56.98 #Float
+greeting = "Hello there" #str
+hasFeathers = True #Boolean
+
+#Data structures
+cars = ["Mercedes","Toyota","Ferrari"] #:List
+fruits = ("Banana","Apples","Orange") #Tuple
+countries = {"England","France","Germany"}#Set
+student = {
+    "firstname" : "Ann",
+    "course" : "MIT",
+    "age" : 19,
+    "nationality" : "Kenyan",
+}
+
+print(number)
+print(num)
+print(greeting)
+print(hasFeathers)
+print(cars)
+print(fruits)
+print(countries)
+print(student["nationality"])
+
+#Typecasting-converting one data type to another
+print(float(number))
+print(int(num))
